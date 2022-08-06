6632679cdc10ee461f86fdf97074522ae384fa33
Stanley Nwaegwu <kingstanley.ks16@gmail.com>
2022-08-06 22:55:53 +0100

    changes in mongo url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/meetDB',
+  MONGO_URL:
+    'mongodb+srv://kingstanley:Nj12063@cluster0.6noj5.mongodb.net/meetDB?retryWrites=true&w=majority',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   TOKEN_KEY: 'verysimpletoken_easy_meet',
-  MONGO_URL: 'mongodb://localhost/meetDB',
+  MONGO_URL:
+    'mongodb+srv://kingstanley:Nj12063@cluster0.6noj5.mongodb.net/meetDB?retryWrites=true&w=majority',
 };
